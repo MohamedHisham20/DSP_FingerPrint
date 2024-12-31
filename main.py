63b004b7f704f46f00dc35023c395a02dd2333d9
salahmohamed <salahmohamed.sm2016@gmail.com>
2024-12-31 16:55:42 +0200

    refactor

--- main.py
+++ main.py
@@ -30,75 +30,53 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.player.setMedia(QMediaContent(QUrl.fromLocalFile(wav_file)))
         self.ui.show()
     def connections(self):
-        self.ui.choose_file.clicked.connect(self.choose_song)
-        self.ui.play_song_btn.clicked.connect(self.play_stop_selected_song)
-        self.ui.choose_file_2.clicked.connect(self.choose_mix_song1)
-        self.ui.play_song_btn_2.clicked.connect(self.play_stop_mix_song1)
-        self.ui.choose_file_3.clicked.connect(self.choose_mix_song2)
-        self.ui.play_song_btn_3.clicked.connect(self.play_stop_mix_song2)
+        self.ui.choose_file.clicked.connect(lambda: self.choose_audio_file(1))
+        self.ui.play_song_btn.clicked.connect(lambda: self.play_stop_song(1))
+        self.ui.choose_file_2.clicked.connect(lambda: self.choose_audio_file(2))
+        self.ui.play_song_btn_2.clicked.connect(lambda: self.play_stop_song(2))
+        self.ui.choose_file_3.clicked.connect(lambda: self.choose_audio_file(3))
+        self.ui.play_song_btn_3.clicked.connect(lambda: self.play_stop_song(3))
 
-
-    def choose_song(self):
+    def choose_audio_file(self, player_id):
         file_dialog = QtWidgets.QFileDialog()
         file_dialog.setNameFilter("Audio files (*.wav *.mp3)")
         file_path, _ = file_dialog.getOpenFileName(self, "Choose Audio File", "", "Audio files (*.wav *.mp3)")
         if file_path:
-            self.selected_song = file_path
-            name = file_path.split("/")[-1]
-            self.ui.song_name.setText(name)
-            self.ui.play_song_btn.show()
-    def choose_mix_song1(self):
-        file_dialog = QtWidgets.QFileDialog()
-        file_dialog.setNameFilter("Audio files (*.wav *.mp3)")
-        file_path, _ = file_dialog.getOpenFileName(self, "Choose Audio File", "", "Audio files (*.wav *.mp3)")
-        if file_path:
-            self.mix_song1 = file_path
-            name = file_path.split("/")[-1]
-            self.ui.song_name_2.setText(name)
-            self.ui.play_song_btn_2.show()
-    def choose_mix_song2(self):
-        file_dialog = QtWidgets.QFileDialog()
-        file_dialog.setNameFilter("Audio files (*.wav *.mp3)")
-        file_path, _ = file_dialog.getOpenFileName(self, "Choose Audio File", "", "Audio files (*.wav *.mp3)")
-        if file_path:
-            self.mix_song2 = file_path
-            name = file_path.split("/")[-1]
-            self.ui.song_name_3.setText(name)
-            self.ui.play_song_btn_3.show()
-    def play_stop_mix_song1(self):
-        if self.mix_song1:
-            if self.ui.play_song_btn_2.text() == "▶":
-                self.ui.play_song_btn_2.setText("◻")
-                self.player_mix1.setMedia(QMediaContent(QUrl.fromLocalFile(self.mix_song1)))
-                self.player_mix1.play()
+            if player_id == 1:
+                self.selected_song = file_path
+                self.ui.song_name.setText(file_path.split("/")[-1])
+                self.ui.play_song_btn.show()
+            elif player_id == 2:
+                self.mix_song1 = file_path
+                self.ui.song_name_2.setText(file_path.split("/")[-1])
+                self.ui.play_song_btn_2.show()
             else:
-                self.ui.play_song_btn_2.setText("▶")
-                self.player_mix1.stop()
+                self.mix_song2 = file_path
+                self.ui.song_name_3.setText(file_path.split("/")[-1])
+                self.ui.play_song_btn_3.show()
+
+    def play_stop_song(self, player_id):
+        if player_id == 1:
+            song = self.selected_song
+            btn = self.ui.play_song_btn
+            player = self.player
+        elif player_id == 2:
+            song = self.mix_song1
+            btn = self.ui.play_song_btn_2
+            player = self.player_mix1
         else:
-            print("No song selected")
-    def play_stop_mix_song2(self):
-        if self.mix_song2:
-            if self.ui.play_song_btn_3.text() == "▶":
-                self.ui.play_song_btn_3.setText("◻")
-                self.player_mix2.setMedia(QMediaContent(QUrl.fromLocalFile(self.mix_song2)))
-                self.player_mix2.play()
-            else:
-                self.ui.play_song_btn_3.setText("▶")
-                self.player_mix2.stop()
-        else:
-            print("No song selected")
-    
+            song = self.mix_song2
+            btn = self.ui.play_song_btn_3
+            player = self.player_mix2
 
-    
-    def play_stop_selected_song(self):
-        if self.selected_song:
-            if self.ui.play_song_btn.text() == "▶":
-                self.ui.play_song_btn.setText("◻")
-                self.player.setMedia(QMediaContent(QUrl.fromLocalFile(self.selected_song)))
-                self.player.play()
+        if song:
+            if btn.text() == "▶":
+                btn.setText("◻")
+                player.setMedia(QMediaContent(QUrl.fromLocalFile(song)))
+                player.play()
             else:
-                self.ui.play_song_btn.setText("▶")
-                self.player.stop()
+                btn.setText("▶")
+                player.stop()
         else:
             print("No song selected")
 
